3cfffee8a5fe3ac22f50fa78500ef410847c9c54
Morgan OShaughnessey <123529383+theMorganObject@users.noreply.github.com>
2024-08-15 19:30:28 +0100

    working commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,63 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+
+import React, { useRef, useState } from 'react';
+import styles from './page.module.scss';
 
 export default function Home() {
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const generateRandomPosition = () => {
+    const top = Math.floor(Math.random() * 100);
+    const left = Math.floor(Math.random() * 100);
+    return { top: `${top}%`, left: `${left}%` };
+  };
+
+  const generateRandomSize = () => {
+    const size = Math.floor(Math.random() * 300) + 100; // Random size between 100px and 400px
+    return { width: `${size}px`, height: `${size}px` };
+  };
+
+  const rings = Array.from({ length: 3 }, (_, i) => {
+    // Reduced to 3 rings
+    const position = generateRandomPosition();
+    const size = generateRandomSize();
+    const style = { ...position, ...size };
+    return <div key={i} className={styles.ring} style={style}></div>;
+  });
+
+  const handlePlay = () => {
+    if (audioRef.current) {
+      audioRef.current.play();
+      setIsPlaying(true);
+    }
+  };
+
+  const handlePause = () => {
+    if (audioRef.current) {
+      audioRef.current.pause();
+      setIsPlaying(false);
+    }
+  };
+
   return (
-    <main className={styles.main}>
-     
-    </main>
+    <div className={styles.watercolorContainer}>
+      <audio
+        ref={audioRef}
+        src='/audio/blue-sharp-2.wav'
+        onPlay={handlePlay}
+        onPause={handlePause}
+      />
+      <button onClick={isPlaying ? handlePause : handlePlay}>
+        {isPlaying ? 'Pause' : 'Play'}
+      </button>
+      <div
+        className={`${styles.watercolorRings} ${
+          isPlaying ? styles.playing : ''
+        }`}
+      >
+        {rings}
+      </div>
+    </div>
   );
 }
