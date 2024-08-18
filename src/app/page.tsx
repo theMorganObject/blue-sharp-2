48edf1dd3eb8020c76ca61708b24c2bad8c25d30
Morgan OShaughnessey <123529383+theMorganObject@users.noreply.github.com>
2024-08-18 11:10:53 +0100

    drop shadow and node version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,7 @@ import {
 export default function Home() {
   const audioRef = useRef<HTMLAudioElement>(null);
   const [isPlaying, setIsPlaying] = useState(false);
+  const [rings, setRings] = useState<JSX.Element[]>([]);
 
   const generateRandomPosition = () => {
     const top = Math.floor(Math.random() * 100);
@@ -20,81 +21,87 @@ export default function Home() {
   };
 
   const generateRandomSize = () => {
-    const size = Math.floor(Math.random() * 200) + 40; // Random size between 40px and 200px
+    const size = Math.floor(Math.random() * 200) + 40;
     return { width: `${size}px`, height: `${size}px` };
   };
 
   const generateRandomBorderWidth = () => {
-    return Math.floor(Math.random() * 7) + 3; // Random border width between 3px and 9px
+    return Math.floor(Math.random() * 7) + 3;
   };
 
   const generateRandomColor = (palette: string[]): string => {
     return palette[Math.floor(Math.random() * palette.length)];
   };
 
-  const renderRings = () => {
-    return Array.from({ length: 3 }, (_, i) => {
-      const position = generateRandomPosition();
-      const size = generateRandomSize();
-      const borderWidth = generateRandomBorderWidth();
-      const blueColor = generateRandomColor(bluePalette);
-      const indigoColor = generateRandomColor(indigoPalette);
-      const cyanColor = generateRandomColor(cyanPalette);
-      const dropShadowColor = generateRandomColor(combinedPalette);
-      const animationName = `fadeInOut-${i}-${Math.random()}`; // Add random element to animation name
+  const generateRing = (i: number) => {
+    const position = generateRandomPosition();
+    const size = generateRandomSize();
+    const borderWidth = generateRandomBorderWidth();
+    const blueColor = generateRandomColor(bluePalette);
+    const indigoColor = generateRandomColor(indigoPalette);
+    const cyanColor = generateRandomColor(cyanPalette);
+    const dropShadowColor = generateRandomColor(combinedPalette);
+    const animationName = `fadeInOut-${i}-${Math.random()}`;
 
-      const style = {
-        ...position,
-        ...size,
-        borderWidth: `${borderWidth}px`,
-        animationName,
-        animationTimingFunction: 'linear',
-        animationDuration: '4s',
-        animationIterationCount: 'infinite',
-        animationFillMode: 'forwards',
-        borderColor: blueColor,
-      };
+    const style = {
+      ...position,
+      ...size,
+      borderWidth: `${borderWidth}px`,
+      borderColor: blueColor,
+      backgroundColor: 'transparent',
+      animationName,
+      animationTimingFunction: 'linear',
+      animationDuration: '27s',
+      animationIterationCount: '1',
+      animationFillMode: 'forwards',
+      filter: `blur(5px) drop-shadow(0 0 10px ${dropShadowColor})`,
+    };
 
-      return (
-        <div
-          key={`${i}-${Math.random()}`}
-          className={styles.ring}
-          style={style}
-        >
-          <style jsx>{`
-            @keyframes ${animationName} {
-              0% {
-                opacity: 0;
-                transform: scale(0.8);
-                border-color: ${blueColor};
-                filter: blur(5px) drop-shadow(0 0 0 ${dropShadowColor});
-              }
-              37.04% {
-                opacity: 1;
-                transform: scale(1.4);
-                border-color: ${indigoColor};
-                filter: blur(5px) drop-shadow(0 0 0 ${dropShadowColor});
-              }
-              50% {
-                filter: blur(5px) drop-shadow(0 0 10px ${dropShadowColor});
-              }
-              100% {
-                opacity: 0;
-                transform: scale(2);
-                border-color: ${cyanColor};
-                filter: blur(10px) drop-shadow(0 0 25px ${dropShadowColor});
-              }
+    return (
+      <div key={`${i}-${Math.random()}`} className={styles.ring} style={style}>
+        <style jsx>{`
+          @keyframes ${animationName} {
+            0% {
+              opacity: 0;
+              transform: scale(0.8);
+              border-color: ${blueColor};
+              filter: blur(5px) drop-shadow(0 0 0 ${dropShadowColor});
             }
-          `}</style>
-        </div>
-      );
-    });
+            37.04% {
+              opacity: 1;
+              transform: scale(1.4);
+              border-color: ${indigoColor};
+              filter: blur(5px) drop-shadow(0 0 0 ${dropShadowColor});
+            }
+            50% {
+              filter: blur(5px) drop-shadow(0 0 10px ${dropShadowColor});
+            }
+            100% {
+              opacity: 0;
+              transform: scale(2);
+              border-color: ${cyanColor};
+              filter: blur(10px) drop-shadow(0 0 25px ${dropShadowColor});
+            }
+          }
+        `}</style>
+      </div>
+    );
   };
 
   const handlePlay = () => {
     if (audioRef.current) {
       audioRef.current.play();
       setIsPlaying(true);
+
+      const generatedRings = Array.from({ length: 27 }, (_, i) =>
+        generateRing(i)
+      );
+
+      generatedRings.forEach((ring, i) => {
+        setTimeout(() => {
+          setRings((prevRings) => [...prevRings, ring]);
+        }, i * 9000);
+      });
     }
   };
 
@@ -102,6 +109,7 @@ export default function Home() {
     if (audioRef.current) {
       audioRef.current.pause();
       setIsPlaying(false);
+      setRings([]);
     }
   };
 
@@ -116,13 +124,7 @@ export default function Home() {
       <button onClick={isPlaying ? handlePause : handlePlay}>
         {isPlaying ? 'Pause' : 'Play'}
       </button>
-      <div
-        className={`${styles.watercolorRings} ${
-          isPlaying ? styles.playing : ''
-        }`}
-      >
-        {isPlaying && renderRings()}
-      </div>
+      <div className={styles.watercolorRings}>{rings}</div>
     </div>
   );
 }
